267eb3e20981bbe27aa17995413b21cc9ba958bb
nishantnepal3 <157234155+nishantnepal3@users.noreply.github.com>
2024-02-12 20:56:26 -0500

    Add files via upload

---
+++ excersice2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write(" Please enter a whole number: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            // Checking if the number is positive, negative, or zero
+            if (number > 0)
+            {
+                Console.Write("The given number is positive");
+            }
+            else if (number < 0)
+            {
+                Console.Write("The given number is negative");
+            }
+            else
+            {
+                Console.Write("The given number is zero");
+            }
+
+            // Check if the number is odd or even
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(" and even.");
+            }
+            else
+            {
+                Console.WriteLine(" and odd.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
